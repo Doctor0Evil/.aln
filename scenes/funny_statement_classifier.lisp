a479d87c29cd1f72d6412d3c3f6e0de9e103cd92
Doctor0Evil <xboxteejaymcfarmer@gmail.com>
2025-08-27 10:12:23 -0700

    Update funny_statement_classifier.lisp

--- scenes/funny_statement_classifier.lisp
+++ scenes/funny_statement_classifier.lisp
@@ -1,17 +1,25 @@
-;; "https://github.com/Doctor0Evil/ALN_Programming_Language.git"
-;; scenes/funny_statement_classifier.lisp
-
 (defpackage :funny-classifier
   (:use :cl)
   (:export :classify-funny))
 
 (in-package :funny-classifier)
 
-(defun classify-funny (statement)
-  (let* ((incongruity-score (detect-incongruity statement))
-         (truth-score (detect-truth kernel statement))
-         (misdirection-score (detect-misdirection statement))
-         (audience-response (simulate-laugh-track statement)))
-    (if (> (+ incongruity-score truth-score misdirection-score audience-response) 2.5)
-        'funny.statement
-        'not.funny)))
+(defparameter *funny-threshold* 2.5
+  "Minimum cumulative score required to classify a statement as funny.")
+
+(defun score-component (label score)
+  (format t "~&[score] ~A: ~,2f~%" label score)
+  score)
+
+(defun classify-funny (statement &key (kernel nil))
+  "Classifies a statement as :funny or :not-funny based on scoring heuristics."
+  (let* ((scores (list
+                  (score-component "Incongruity" (detect-incongruity statement))
+                  (score-component "Truth" (detect-truth kernel statement))
+                  (score-component "Misdirection" (detect-misdirection statement))
+                  (score-component "Audience Response" (simulate-laugh-track statement))))
+         (total-score (reduce #'+ scores)))
+    (format t "~&[total-score] ~,2f~%" total-score)
+    (if (> total-score *funny-threshold*)
+        :funny
+        :not-funny)))
