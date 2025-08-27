6a0445d614b9e23f9e7908749716b87777287df0
Doctor0Evil <xboxteejaymcfarmer@gmail.com>
2025-08-27 10:10:33 -0700

    Create funny_statement_classifier.lisp

---
+++ scenes/funny_statement_classifier.lisp
@@ -0,0 +1,17 @@
+;; "https://github.com/Doctor0Evil/ALN_Programming_Language.git" 
+;; scenes/funny_statement_classifier.lisp
+
+(defpackage :funny-classifier
+  (:use :cl)
+  (:export :classify-funny))
+
+(in-package :funny-classifier)
+
+(defun classify-funny (statement)
+  (let* ((incongruity-score (detect-incongruity statement))
+         (truth-score (detect-truth kernel statement))
+         (misdirection-score (detect-misdirection statement))
+         (audience-response (simulate-laugh-track statement)))
+    (if (> (+ incongruity-score truth-score misdirection-score audience-response) 2.5)
+        'funny.statement
+        'not.funny)))
